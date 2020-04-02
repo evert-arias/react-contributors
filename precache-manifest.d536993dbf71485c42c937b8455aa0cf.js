ba3b669f4e79a79ed1141fa9c1845af5bbf51894
Evert Arias <evert.arias@hotmail.com>
2020-04-02 01:50:41 -0400

    Updates

---
+++ precache-manifest.d536993dbf71485c42c937b8455aa0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a498e8be29af8e2da21b94f0a5ef3130",
+    "url": "/react-contributors/index.html"
+  },
+  {
+    "revision": "7e60b3ec9a78b6d20234",
+    "url": "/react-contributors/static/css/main.0cece520.chunk.css"
+  },
+  {
+    "revision": "da23ac92f30eca1c9b93",
+    "url": "/react-contributors/static/js/2.2d2ea67c.chunk.js"
+  },
+  {
+    "revision": "fbdb25c6a30511ca0525a561a8e4c009",
+    "url": "/react-contributors/static/js/2.2d2ea67c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e60b3ec9a78b6d20234",
+    "url": "/react-contributors/static/js/main.81a1e2ae.chunk.js"
+  },
+  {
+    "revision": "29211258c567b932b52c",
+    "url": "/react-contributors/static/js/runtime-main.2917b01d.js"
+  }
+]);
