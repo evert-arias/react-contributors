2a9740c2164e5778d43146f74fe69f892eb70b54
Evert Arias <evert.arias@hotmail.com>
2020-04-02 02:55:37 -0400

    Remove unnecessary css file import

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Contributors from "react-contributors";
-import "react-contributors/dist/index.css";
 
 const App = () => {
   return <Contributors owner="evert-arias" repo="EasyButton" />;
