8ddd7ab1815e0c0e4cbd52861f0840d22e10b792
Evert Arias <evert.arias@hotmail.com>
2020-04-02 18:17:13 -0400

    Use multiple repos which is now supported by the library

--- example/src/App.js
+++ example/src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 
 import Contributors from "react-contributors";
 
+// Owner username.
+const owner = "evert-arias";
+
+// Repo(s) name. Single string or array with multiple repo names.
+const repos = ["EasyButton", "easybtn.earias.me"];
+
 const App = () => {
-  return <Contributors owner="evert-arias" repo="EasyButton" />;
+  return <Contributors owner={owner} repo={repos} />;
 };
 
 export default App;
