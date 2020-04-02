6b2bce916be3f72fb97c34aca1bf971510eee544
Evert Arias <evert.arias@hotmail.com>
2020-04-02 02:54:51 -0400

    Remove css and add styled-components

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from "react";
 import { Octokit } from "@octokit/rest";
-import styles from "./styles.module.css";
+// import styles from "./styles.module.css";
+import { Contributors, Profile, Photo, Intro, Name, Subtitle } from "./styles";
 
 const octokit = new Octokit();
 
-class Profile extends React.Component {
+class ProfileComponent extends React.Component {
   render() {
     return (
-      <div className={styles.profile}>
-        <img
-          className={`${styles.photo} ${styles["photo-lg"]}`}
-          src={this.props.avatar_url}
-        />
-        <div className={styles.intro}>
-          <h4 className={styles.name}>{this.props.name}</h4>
-          <small className={styles.subtitle}>
+      <Profile>
+        <Photo src={this.props.avatar_url} />
+        <Intro>
+          <Name>{this.props.name}</Name>
+          <Subtitle>
             <a href={this.props.url}>{this.props.url}</a>
-          </small>
-        </div>
-      </div>
+          </Subtitle>
+        </Intro>
+      </Profile>
     );
   }
 }
 
-export default class Contributors extends React.Component {
+export default class ContributorsComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -53,11 +51,11 @@ export default class Contributors extends React.Component {
 
   render() {
     return (
-      <div className={styles.contributors}>
+      <Contributors>
         {this.state.contributors && this.state.contributors.length
           ? this.state.contributors.map((contributor, key) => {
               return (
-                <Profile
+                <ProfileComponent
                   key={key}
                   avatar_url={contributor.avatar_url}
                   name={contributor.login}
@@ -66,7 +64,7 @@ export default class Contributors extends React.Component {
               );
             })
           : undefined}
-      </div>
+      </Contributors>
     );
   }
 }
